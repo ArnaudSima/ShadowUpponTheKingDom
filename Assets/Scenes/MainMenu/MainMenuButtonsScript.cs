18d517793848a1638e07fb60d617097b049b2588
Arnaud Simard Desmeules <124740290+ArnaudSima@users.noreply.github.com>
2025-12-08 21:48:21 -0500

    commencement du menu

---
+++ Assets/Scenes/MainMenu/MainMenuButtonsScript.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtonsScript : MonoBehaviour
+{
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+}
